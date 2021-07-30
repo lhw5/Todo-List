e4b64830d7a379eee67aeeed7f479e3261a8d456
lhw5 <203658975@qq.com>
2021-07-30 16:31:13 +0800

    上传dist

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,203 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader @addTodo="addTodo" />
+        <MyList :todos="todos" />
+        <MyFooter :todos="todos" @checkAllTodo="checkAllTodo" @clearAllTodo="clearAllTodo" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from "./components/MyHeader";
+import MyList from "./components/MyList";
+import MyFooter from "./components/MyFooter";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    MyHeader,
+    MyList,
+    MyFooter
+  },
+  data() {
+    return {
+      todos: JSON.parse(localStorage.getItem("todos")) || []
+    };
+  },
+  methods: {
+    //添加一个todo
+    addTodo(todoObj) {
+      let flag = true;
+      this.todos.map(item => {
+        if (item.title === todoObj.title) {
+          flag = false;
+          alert("任务已存在!");
+        }
+      });
+      if (flag) {
+        this.todos.unshift(todoObj);
+      }
+      //this.todos.unshift(todoObj)
+    },
+    //勾选or取消勾选一个todo
+    checkTodo(id) {
+      this.todos.forEach(todo => {
+        if (todo.id === id) todo.completed = !todo.completed;
+      });
+    },
+    //删除一个todo
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    //全选or取消全选
+    checkAllTodo(completed) {
+      this.todos.forEach(todo => {
+        todo.completed = completed;
+      });
+    },
+    //删除所有已经完成的todo
+    clearAllTodo() {
+      this.todos = this.todos.filter(todo => {
+        return !todo.completed;
+      });
+    },
+    //更新一个todo
+    updateTodo(id, title) {
+      this.todos.forEach(todo => {
+        if (todo.id === id) {
+          let flag = true;
+          this.todos.map(item => {
+            if (item.title === title) {
+              flag = false;
+              alert("任务已存在!");
+            }
+          });
+          if (flag) {
+            todo.title = title;
+          }
+        }
+      });
+    }
+  },
+  watch: {
+    todos: {
+      deep: true,
+      handler(value) {
+        localStorage.setItem("todos", JSON.stringify(value));
+      }
+    }
+  },
+  mounted() {
+    this.$bus.$on("checkTodo", this.checkTodo);
+    this.$bus.$on("deleteTodo", this.deleteTodo);
+    this.$bus.$on("updateTodo", this.updateTodo);
+  },
+  beforeDestroy() {
+    this.$bus.$off("checkTodo");
+    this.$bus.$off("deleteTodo");
+    this.$bus.$off("updateTodo");
   }
-}
+};
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 hsla(0, 0%, 100%, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #da4f45;
+}
+
+.btn-edit {
+  color: #fff;
+  margin-right: 2px;
+  background-color: #45c745;
+  border: 1px solid #5ab66c;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #dd2e25;
+}
+
+.btn-edit:hover {
+  color: #fff;
+  background-color: #45ce45;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+  background: rgba(156, 190, 255, 0.9);
+}
+
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+input[type="checkbox"] {
+  cursor: pointer;
+  position: relative;
+}
+
+input[type="checkbox"]:after {
+  position: absolute;
+  top: 0;
+  background-color: #fff;
+  color: #fff;
+  width: 14px;
+  height: 14px;
+  display: inline-block;
+  visibility: visible;
+  padding-left: 0;
+  text-align: center;
+  content: " ";
+  border-radius: 2px;
+  box-sizing: border-box;
+  border: 1px solid #ddd;
+}
+
+input[type="checkbox"]:checked:after {
+  content: "";
+  border-color: #037df3;
+  background-color: #037df3;
+}
+
+input[type="checkbox"]:checked:before {
+  content: "";
+  position: absolute;
+  top: 1px;
+  left: 5px;
+  width: 3px;
+  height: 8px;
+  border: solid #fff;
+  border-width: 0 2px 2px 0;
+  transform: rotate(45deg);
+  z-index: 1;
 }
 </style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="todo-header">
+    <input type="text" placeholder="请输入任务名称，敲回车键确认!" v-model="title" autofocus @keyup.enter="add" />
+  </div>
+</template>
+
+<script>
+import { nanoid } from "nanoid";
+export default {
+  name: "MyHeader",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    add() {
+      if (!this.title.trim()) return alert("输入不能为空!");
+      const todoObj = {
+        id: nanoid(),
+        title: this.title.trim(),
+        completed: false
+      };
+      this.$emit("addTodo", todoObj);
+      this.title = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.todo-header input {
+  width: 560px;
+  height: 28px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 8px;
+}
+
+.todo-header input:focus {
+  outline: none;
+  border-color: rgba(82, 168, 236, 0.8);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075),
+    0 0 8px rgba(82, 168, 236, 0.6);
+}
+</style>
